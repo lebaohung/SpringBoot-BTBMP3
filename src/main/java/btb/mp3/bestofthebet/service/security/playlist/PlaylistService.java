fb3a0f8ce58e3186d6684611a261a8647234f859
Harry Nguyen <50328363+HarryNguyen99@users.noreply.github.com>
2020-07-22 20:46:40 +0700

    PlaylistService

--- src/main/java/btb/mp3/bestofthebet/service/security/playlist/PlaylistService.java
+++ src/main/java/btb/mp3/bestofthebet/service/security/playlist/PlaylistService.java
@@ -1,17 +1,17 @@
-//package btb.mp3.bestofthebet.service.security.playlist;
-//
-//import btb.mp3.bestofthebet.model.PlayList;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//
-//import java.util.Optional;
-//
-//public interface PlaylistService {
-//    Page<PlayList> findAll(Pageable pageable);
-//
-//    Optional<PlayList> findById(Long id);
-//
-//    void save(PlayList playList);
-//
-//    void remove(Long id);
-//}
+package btb.mp3.bestofthebet.service.security.playlist;
+
+import btb.mp3.bestofthebet.model.PlayList;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface PlaylistService {
+    Page<PlayList> findAll(Pageable pageable);
+
+    Optional<PlayList> findById(Long id);
+
+    void save(PlayList playList);
+
+    void remove(Long id);
+}
