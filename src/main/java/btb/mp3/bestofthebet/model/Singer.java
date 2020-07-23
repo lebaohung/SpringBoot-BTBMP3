132e5f4ca4e6561d4804fe7667835d5c99f727b2
Harry Nguyen <50328363+HarryNguyen99@users.noreply.github.com>
2020-07-23 10:10:42 +0700

    add Entity image Singer

--- src/main/java/btb/mp3/bestofthebet/model/Singer.java
+++ src/main/java/btb/mp3/bestofthebet/model/Singer.java
@@ -12,8 +12,14 @@ public class Singer {
 
     @Id
     @GeneratedValue (strategy = GenerationType.AUTO)
-
+    @Column (nullable = false)
     private Long id;
+    @Column (nullable = false)
     private String name;
+
+    @Column (nullable = false)
+    private String image;
+
+    @Column (nullable = false)
     private Date createDate;
 }
