c595a1119b7c6c1a605b3ada0a5e68f31f86c193
CaoXuanQuy1985 <cun.kin1985.working@gmail.com>
2020-07-25 13:15:07 +0700

    add file CommentPlayListServiceImpl

---
+++ src/main/java/btb/mp3/bestofthebet/service/commentPlayListService/impl/CommentPlayListServiceImpl.java
@@ -0,0 +1,34 @@
+package btb.mp3.bestofthebet.service.commentPlayListService.impl;
+
+import btb.mp3.bestofthebet.model.Comment_Playlist;
+import btb.mp3.bestofthebet.repository.CommentPlayListRepository;
+import btb.mp3.bestofthebet.service.commentPlayListService.ICommentPlayListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CommentPlayListServiceImpl implements ICommentPlayListService {
+
+    @Autowired
+    private CommentPlayListRepository commentPlayListRepository;
+
+    public List<Comment_Playlist> findAll() {
+        return null;
+    }
+
+    public Optional<Comment_Playlist> findById(Long id) {
+        return null;
+    }
+
+    public void save(Comment_Playlist commentSong) {
+    }
+
+    public void delete(Long id) {}
+
+    public List<Comment_Playlist> deleteByUserId(Long id) {
+        return commentPlayListRepository.deleteByUserId(id);
+    }
+}
