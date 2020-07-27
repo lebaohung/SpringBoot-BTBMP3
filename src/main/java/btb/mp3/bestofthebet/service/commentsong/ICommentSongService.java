32c0d871f564895eea3ae4d2e3103a877e92c9d9
TuanVu321 <tuantnmt1@gmail.com>
2020-07-27 23:23:36 +0700

    them api viet comment vao song va xem comment

--- src/main/java/btb/mp3/bestofthebet/service/commentsong/ICommentSongService.java
+++ src/main/java/btb/mp3/bestofthebet/service/commentsong/ICommentSongService.java
@@ -1,6 +1,7 @@
 package btb.mp3.bestofthebet.service.commentsong;
 
 import btb.mp3.bestofthebet.model.Comment_Song;
+import btb.mp3.bestofthebet.model.Song;
 import btb.mp3.bestofthebet.service.IService;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface ICommentSongService extends IService<Comment_Song> {
 
     List<Comment_Song> deleteByUserId(Long userId);
+    List<Comment_Song> showCommentsBySong(Song song);
 }
