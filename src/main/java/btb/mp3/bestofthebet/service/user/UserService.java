acbd191a69cdf47e1d7eac61f6736166a5c7ac47
CaoXuanQuy1985 <cun.kin1985.working@gmail.com>
2020-07-29 08:53:50 +0700

    Creating UserService

--- src/main/java/btb/mp3/bestofthebet/service/user/UserService.java
+++ src/main/java/btb/mp3/bestofthebet/service/user/UserService.java
@@ -1,9 +1,15 @@
 package btb.mp3.bestofthebet.service.user;
 
 import btb.mp3.bestofthebet.model.User;
+import btb.mp3.bestofthebet.service.IService;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
-public interface UserService {
+public interface UserService extends IService<User> {
+
     User finByID(Long id);
+
+    List<Object> statisticUsersByCreateDate();
 }
