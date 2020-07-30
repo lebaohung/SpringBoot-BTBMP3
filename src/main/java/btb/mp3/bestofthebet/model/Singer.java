f0436099584bafe1b7b7bce30271e2d2c27de9d0
Vu Dai Duong <daiduongutc97@gmail.com>
2020-07-30 14:44:27 +0700

    Edit Model

--- src/main/java/btb/mp3/bestofthebet/model/Singer.java
+++ src/main/java/btb/mp3/bestofthebet/model/Singer.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class Singer {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Column (nullable = false)
     private Long id;
     private String name;
--- src/main/java/btb/mp3/bestofthebet/model/Singer_And_song.java
+++ src/main/java/btb/mp3/bestofthebet/model/Singer_And_song.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class Singer_And_song {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Column (nullable = false)
     private Long id;
 
